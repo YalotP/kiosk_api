f1b027e3af95c34d7373564948833b5b6193f51a
YalotP <94488010+YalotP@users.noreply.github.com>
2022-04-27 13:26:09 +0300

    Add files via upload

--- index.js
+++ index.js
@@ -3,23 +3,26 @@ const mongoose = require('mongoose');
 const bodyParser = require('body-parser');
 const app = express();
 
-app.use(bodyParser.urlencoded());
+app.use(bodyParser.urlencoded({extends:false}));
 app.use(bodyParser.json());
 
-const accountRoute = require('./controllers/accounts');
-app.use('/api/accounts', accountRoute);
-app.use('/api/store',storeRoute);
+const accountsRoute = require('./controllers/accounts');
+const storeRoute = require('./controllers/store');
+const productRoute = require('./controllers/product');
+app.use('/api/accounts', accountsRoute);
+app.use('/api/store', storeRoute);
+app.use('/api/product', productRoute);
 
 const port = 5090;
 
-const url = 'mongodb+srv://kiosk_user:KnQH24QPnblMDqbf@cluster0.zz82a.mongodb.net/myFirstDatabase?retryWrites=true&w=majority';
+const url = 'mongodb+srv://kiosk_user:mPfFB6de1GkUzMEf@cluster0.cxcp4.mongodb.net/kiosk_db?retryWrites=true&w=majority';
 mongoose.connect(url)
 .then(results => {
     console.log(results);
-    app.listen(port,function(){
-        console.log(`Server is running via port ${port}`);
+    app.listen(port, function(){
+        console.log(`Server is runing via port ${port}`);
     })
 })
-.catch(err =>{
+.catch(err => {
     console.log(err);
 })
